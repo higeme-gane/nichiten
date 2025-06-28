fc16771e89d4374a66f3242a0e71f5f107691e64
higeme-gane <rr34vv@gmail.com>
2025-06-28 15:36:07 +0900

    second_png

--- nichiten_map_20250622.R
+++ nichiten_map_20250622.R
@@ -1,14 +1,37 @@
 rm(list = ls())
 library(conflicted)
 library(tidyverse)
-library(openxlsx)
 library(psych)
 conflicts_prefer(dplyr::filter)
 
 popular_rate <- 3
 unpopular_rate <- -3
 
-df_long <- read_csv("nichiten_loto_2025.csv") |> 
+df <- read_csv("nichiten_loto_2025.csv")
+head(arrange(.data = df, value), n = 10)
+head(arrange(.data = df, desc(value)), n = 10)
+df_describe <- describe(df$value)
+df_describe
+
+n <- df_describe$n 
+n_expected <- df_describe$mean
+
+g <- ggplot(df, aes(x = value))
+# g <- g + geom_rect(xmin = ceiling(unpopular_boundary), xmax = floor(popular_boundary),
+#             ymin = -Inf, ymax = Inf, fill = "pink", alpha = 0.5)
+g <- g + geom_histogram(binwidth = 1, fill = "blue", color = "black")
+g <- g + geom_vline(xintercept = n_expected, color = "purple", linetype = "dashed")
+g <- g + labs(title = "Histogram of Values with Mean", x = "Value", y = "Frequency")
+plot(g)
+ggsave("histgram.png", plot = g,
+       width = 2039,
+       height = 1447,
+       units = c("px"))
+
+unpopular_boundary <- unpopular_rate * sqrt(n_expected) + n_expected
+popular_boundary <- popular_rate * sqrt(n_expected) + n_expected
+
+df_long <- df |> 
   mutate(num_pad = str_pad(num, width = 3, side = c("left"), pad = "0")) |> 
   mutate(range1 = str_sub(num_pad, 1, 1),
          range2 = str_sub(num_pad, 2, 2),
@@ -19,15 +42,7 @@ df_long <- read_csv("nichiten_loto_2025.csv") |>
          same_100_10 = if_else(range1 == range2, 1, 0),
          same_10_1 = if_else(range2 == range3, 1, 0),
          same_100_1 = if_else(range1 == range3, 1, 0),
-         same_2 = if_else(range1 != range2 & range2 != range3 & range1 != range3, 0, 1))
-
-n_applicants <- sum(df_long$value)
-n <- nrow(df_long) 
-n_expected = n_applicants / n
-unpopular_boundary <- unpopular_rate * sqrt(n_expected) + n_expected
-popular_boundary <- popular_rate * sqrt(n_expected) + n_expected
-
-df_long <- df_long |> 
+         same_2 = if_else(range1 != range2 & range2 != range3 & range1 != range3, 0, 1)) |> 
   mutate(residuals = (value - n_expected) / sqrt(n_expected)) |> 
   mutate(rates = case_when(
     residuals > popular_rate ~ "popular",
@@ -35,15 +50,16 @@ df_long <- df_long |>
     .default = "standard"
   ))
 
-g <- ggplot(df_long, aes(x = value)) +
-  geom_rect(xmin = unpopular_boundary, xmax = popular_boundary, ymin = -Inf, ymax = Inf, fill = "pink", alpha = 0.5) +
-  geom_histogram(binwidth = 1, fill = "blue", color = "black") +
-  geom_vline(xintercept = n_expected, color = "purple", linetype = "dashed") +
-  labs(title = "Histogram of Values with Mean", x = "Value", y = "Frequency")
-plot(g)
-
 piv_popular <- df_long |> 
   summarise(value = n(), .by = "rates")
+
+n_unpopular <- piv_popular |> filter(rates == "unpopular") |> pull(value)
+n_standard <- piv_popular |> filter(rates == "standard") |> pull(value)
+n_popular <- piv_popular |> filter(rates == "popular") |> pull(value)
+rate_unpopular <- n_unpopular / n
+rate_standard <- n_standard / n
+rate_popular <- n_popular / n
+
 mat_popular <- piv_popular |> 
   mutate(range = "all")
 
@@ -95,7 +111,8 @@ df_wide <- map(vec_range, ~{
 }) |> list_rbind() |> 
   select(range, item, unpopular, standard, popular) |> 
   left_join(df_p_value, by = c("range", "item")) |> 
-  mutate(rates = case_when(unpopular > popular ~ "unpopular",
-                           unpopular < popular ~ "popular",
-                           .default = "even"))
+  mutate(rates = if_else(unpopular / (unpopular + standard + popular) > rate_unpopular,
+                         "unpopular", "not_unpopular"))
 # write_csv(df_wide, "wide_p_value.csv")
+df_unpopular <- df_wide |> 
+  filter(sig_dif == 1, rates == "unpopular")
